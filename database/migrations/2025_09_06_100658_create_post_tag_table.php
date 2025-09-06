5fdc051a60b0018aeb6e8e02b71a14eb081b7dfb
Alan Ambrose <alanambrosedev@gmail.com>
2025-09-06 15:58:21 +0530

    feat: implement Post, Category, and Tag models with factories and migrations

--- database/migrations/2025_09_06_100658_create_post_tag_table.php
+++ database/migrations/2025_09_06_100658_create_post_tag_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('article_tag', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('article_id')->constrained();
-            $table->foreignId('tag_id')->constrained();
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('article_tag');
+        Schema::dropIfExists('post_tag');
     }
 };
